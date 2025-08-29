5e4493b9093ba672fece6ecaa1fa13f9252317db
samuel <00023020@uca.edu.sv>
2025-08-29 04:23:41 -0600

    👔 feat: add user and auth module structure

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(userInfo: CreateUserDTO): Promise<User | null> {
+    const user = await this.prisma.user.create({
+      data: userInfo,
+    });
+    return user;
+  }
+}
