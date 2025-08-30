ba442536e87990fdf8f452db67eed34dbfa223bb
samuel <00023020@uca.edu.sv>
2025-08-29 23:33:57 -0600

    🛂 Finish implementing JWT based auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,12 @@ import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
+import { UsersService } from 'src/users/users.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [UsersModule, JwtModule.registerAsync(jwtConfig.asProvider()), ConfigModule.forFeature(jwtConfig)],
-  providers: [AuthService],
+  providers: [AuthService, UsersService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
